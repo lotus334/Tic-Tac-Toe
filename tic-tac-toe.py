4b0fa7c21c1f7eb0f9397c9bc526d47976fa9544
Васильев Дмитрий <lotus33497@yandex.ru>
2020-08-19 19:18:44 +0700

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -5,8 +5,29 @@ Created on Wed Aug 19 17:48:21 2020
 @author: OKS-8
 """
 
-print('''
-X O X
-O X O
-X X O
-''')
+first_line = []
+second_line = []
+third_line = []
+
+inp = input('Enter cells: ')
+
+for element in range(0,3):
+    first_line.append(inp[element])
+for element in range(3,6):
+    second_line.append(inp[element])
+for element in range(6,9):
+    third_line.append(inp[element])
+
+print('---------')
+print('| ' + ' '.join(first_line) + ' |')
+print('| ' + ' '.join(second_line) + ' |')
+print('| ' + ' '.join(third_line) + ' |')
+print('---------')
+
+
+'''
+print('|' + ''join.O _ O |
+| X X O |
+| _ X X |
+---------
+'''
