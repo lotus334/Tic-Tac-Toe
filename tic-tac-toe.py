26a2261f75a54185c24750b4314a2fa5e960ed70
Васильев Дмитрий <lotus33497@yandex.ru>
2020-08-20 14:01:40 +0700

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -32,20 +32,34 @@ def check_the_rows(matrix):
     return False
 
 def check_the_column(matrix):
-    inline = 0
+#    inline = 0
     for index in range(len(matrix[0])):
-        for row in matrix:
-            if row[index] == row[index - 1]:
+        inline = 0
+        for j in range(len(matrix)):
+            if matrix[j][index] == matrix[j-1][index]:
                 inline += 1
-        if inline == 3:
+        if inline == 3 and matrix[j][index] != '_':
             global winer
-            winer = row[index]
+            winer = matrix[j][index]
             return True
     return False
 
-print(matrix)
+def check_the_diagonal(matrix):
+    if (matrix[0][0] == matrix[1][1] and matrix[1][1] == matrix[2][2]) or (matrix[0][2] == matrix[1][1] and matrix[1][1] == matrix[2][0]) and matrix[1][1] != '_':
+        global winer
+        winer = matrix[1][1]
+        return True
+    return False
 
-if check_the_rows(matrix) or check_the_column(matrix):
+def check_the_empty(matrix):
+    for row in matrix:
+        if '_' in row:
+            return False
+    return True
+
+if check_the_rows(matrix) or check_the_column(matrix) or check_the_diagonal(matrix):
     print(f'{winer} wins')
+elif check_the_empty(matrix):
+    print('Draw')
 else:
     print('None')
