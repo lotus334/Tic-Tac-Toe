ae76fe177a3a7105858e33384719928ff3f79284
Васильев Дмитрий <lotus33497@yandex.ru>
2020-08-20 12:59:56 +0700

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -5,29 +5,35 @@ Created on Wed Aug 19 17:48:21 2020
 @author: OKS-8
 """
 
-first_line = []
-second_line = []
-third_line = []
-
 inp = input('Enter cells: ')
 
-for element in range(0,3):
-    first_line.append(inp[element])
-for element in range(3,6):
-    second_line.append(inp[element])
-for element in range(6,9):
-    third_line.append(inp[element])
+first_line = [inp[i] for i in range(0, 3)]
+second_line = [inp[i] for i in range(3, 6)]
+third_line = [inp[i] for i in range(6, 9)]
+matrix = [first_line, second_line, third_line]
 
-print('---------')
-print('| ' + ' '.join(first_line) + ' |')
-print('| ' + ' '.join(second_line) + ' |')
-print('| ' + ' '.join(third_line) + ' |')
-print('---------')
+print('---------' + '\n'
+      '| ' + ' '.join(first_line) + ' |' + '\n'
+      '| ' + ' '.join(second_line) + ' |' + '\n'
+      '| ' + ' '.join(third_line) + ' |' + '\n'
+      '---------'
+      )
 
+def check_the_rows(matrix):
+    for line in matrix:
+        inline = 1
+        for index in range(len(line)):
+            if line[index] == line[index-1]:
+                inline += 1
+        if inline >= 3 and line[index] != '_':
+            global winer
+            winer= line[index]
+            return True
+    return False
 
-'''
-print('|' + ''join.O _ O |
-| X X O |
-| _ X X |
----------
-'''
+print(matrix)
+
+if check_the_rows(matrix) == True:
+    print(f'{winer} wins')
+else:
+    print('None')
